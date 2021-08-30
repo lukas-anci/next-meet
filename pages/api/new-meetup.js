060a5bd40d0a6598325c873ba03c9f3534084e36
Lukas <lukas.ancikevicius@gmail.com>
2021-08-30 13:13:48 +0300

    new meetup api logic

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 // localhost:3000/api/new-meetup
 // cia aprasyti galima prisijungimo slaptazodzius ir kita susijusia info
 // cia aprasytas kodas nekeliauja i client narsykle
-// mongodb+srv://lukasAdmin:<password>@frankfurtclusteraws.xj0nq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb+srv://lukasAdmin:<password>@frankfurtclusteraws.xj0nq.mongodb.net/meetupDb?retryWrites=true&w=majority
 async function handler(req, res) {
   console.log(req.method);
 
@@ -11,9 +11,20 @@ async function handler(req, res) {
     const data = req.body;
     console.log('got data in api/new-meetup', data);
 
-    const client = await MongoClient.connect('your connection');
-    client.close();
-    res.status(200).json({ msg: 'success', data });
+    try {
+      const client = await MongoClient.connect('your connection');
+      const db = client.db();
+
+      // sukurti arba nusitaikyti i esama
+
+      const meetupCollection = db.collection('meetups');
+      const insertResult = await meetupCollection.insertOne(data);
+      res.status(201).json({ msg: 'success', insertResult });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    } finally {
+      client.close();
+    }
   }
 }
 
