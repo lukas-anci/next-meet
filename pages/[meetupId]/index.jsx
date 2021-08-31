36443a24c4ba12a50b0d80eff88cc44b864d9399
Lukas <lukas.ancikevicius@gmail.com>
2021-08-31 09:51:59 +0300

    getCollection to use inside getStaticPaths

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,4 +1,6 @@
 import MeetupDetail from './../../components/meetups/MeetupDetail';
+// import { MongoClient } from 'mongodb';
+import { getCollection } from './../../utils/mongo-data';
 
 const MeetupDetails = (props) => {
   return (
@@ -13,23 +15,31 @@ const MeetupDetails = (props) => {
   );
 };
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
   // nurodo pagal kokia dinamine informacija sugeneruoti statinius puslapius
+  // const client = await MongoClient.connect(process.env.MONGO_CONN);
+  // const db = client.db();
+
+  // // sukurti arba nusitaikyti i esama
+
+  // const meetupCollection = db.collection('meetups');
+  // aprasyti prisijungimo logika ir gauti reikiamas reiksmes
+  const [meetupCollection, client] = await getCollection();
+  // const meetupCollection = arr[0];
+  const allMeets = await meetupCollection.find({}).toArray();
+  client.close();
+  const pathsArrOfCurrentMeets = allMeets.map((dbObj) => {
+    return {
+      params: {
+        meetupId: dbObj._id.toString(),
+      },
+    };
+  });
+  console.log('all meets', pathsArrOfCurrentMeets);
   return {
     fallback: false, //TRUE: jei einam i psl kurio nera aprasyta path tai tas  psl sugeneruojamas uzklausos metu
     // False: jei einam i psl kurio nera aprasyta paths tai gaunam 404
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    paths: pathsArrOfCurrentMeets,
   };
 }
 
---
+++ utils/mongo-data.js
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+export async function getCollection() {
+  const client = await MongoClient.connect(process.env.MONGO_CONN);
+  const db = client.db();
+
+  // sukurti arba nusitaikyti i esama
+
+  const meetupCollection = db.collection('meetups');
+
+  return [meetupCollection, client];
+}
